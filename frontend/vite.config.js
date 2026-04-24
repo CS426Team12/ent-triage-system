100a2d2ced49deb2c1817680960b61d82d052363
Kyla Trotter <ktrotter@unr.edu>
2026-04-23 19:35:22 -0700

    fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,5 +15,10 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 8080,
+    allowedHosts: ['team12.unr.dev', 'localhost', '127.0.0.1']
   }
 });
